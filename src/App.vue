c8b7ab20e29ee7a55ed88fca028cd7aa8b75762f
yes <myinfestedmind@gmail.com>
2019-12-14 15:05:21 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <user-card></user-card>
   </div>
 </template>
 
 <script>
-
+import UserCard from './components/UserCard.vue'
 export default {
   name: 'app',
   components: {
-    
+    UserCard
   }
 }
 </script>
